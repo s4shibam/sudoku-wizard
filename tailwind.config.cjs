09eeb0ab2e53cbfe73abcb1b3f73ee7d5b1525dc
s4shibam <shibamsaha2002@gmail.com>
2023-09-03 02:56:47 +0530

    feat: applied loading animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ['*'],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        poppins: ['Poppins', 'sans-serif']
       },
-    },
+      animation: {
+        reveal: 'reveal .2s ease-in'
+      },
+      keyframes: {
+        reveal: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
